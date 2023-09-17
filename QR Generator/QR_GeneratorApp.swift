b4a8c2e62233c5942bff22ce7841ec5f90da0a74
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-17 15:35:33 +0530

    Initial Commit

---
+++ QR Generator/QR_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_GeneratorApp.swift
+//  QR Generator
+//
+//  Created by Rishi Singh on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct QR_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
